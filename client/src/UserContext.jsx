a65a4a481350c0a8aecac2ef6136bd633da9fdf4
Suswetha6 <140714308+Suswetha6@users.noreply.github.com>
2024-10-21 18:32:43 +0530

    commit1

---
+++ client/src/UserContext.jsx
@@ -0,0 +1,22 @@
+import axios from "axios";
+import { useState,createContext,useEffect } from "react";
+
+
+export const UserContext = createContext({});
+
+export function UserContextProvider({children}){
+  const [user,setUser] = useState(null);
+  useEffect( ()=>{
+    if(!user){
+      const data = axios.get('/profile').then(({data})=>{
+        setUser(data);
+      });
+      
+    }  },[]);
+  return(
+    <UserContextProvider value={{user,setUser}}>
+      {children}
+    </UserContextProvider>
+    
+  )
+}
